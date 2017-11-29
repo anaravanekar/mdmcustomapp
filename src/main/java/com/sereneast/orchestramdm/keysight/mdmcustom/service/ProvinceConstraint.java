0c4c636b8d9fb5e9d7c832c237a29041b0f77168
anaravanekar <anaravanekar@serenecorp.com>
2017-11-29 14:49:43 +0530

    Send Acknowledgement Invoice Copies for JA, State and Province Constraint, addedd actuator

--- src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/service/ProvinceConstraint.java
+++ src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/service/ProvinceConstraint.java
@@ -8,13 +8,12 @@ import com.sereneast.orchestramdm.keysight.mdmcustom.Paths;
 
 import java.util.Locale;
 
-public class StateConstraint implements Constraint, ConstraintOnNull {
+public class ProvinceConstraint implements Constraint, ConstraintOnNull {
     @Override
     public void checkNull(ValueContextForValidation valueContextForValidation) throws InvalidSchemaException {
         Object countryValue = valueContextForValidation.getValue(Path.PARENT.add(Paths._Address._Country));
-        if(countryValue!=null && ("BR".equals(String.valueOf(countryValue)) || "US".equals(String.valueOf(countryValue))
-        || "IN".equals(String.valueOf(countryValue)) || "MX".equals(String.valueOf(countryValue)))){
-            UserMessage message = UserMessage.createError("State is required for Brazil, United States, India and Mexico");//UserMessage(Severity.ERROR,"ThePriceMustNotDifferFromTheDefaultPriceMoreThan{0}");
+        if(countryValue!=null && ("CA".equals(String.valueOf(countryValue)))){
+            UserMessage message = UserMessage.createError("Province is required for Canada");//UserMessage(Severity.ERROR,"ThePriceMustNotDifferFromTheDefaultPriceMoreThan{0}");
             valueContextForValidation.addMessage(message);
         }
     }
