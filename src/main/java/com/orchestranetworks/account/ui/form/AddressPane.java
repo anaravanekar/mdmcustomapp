0eb83b5782c58d8e47c22920c3cc3c9b05689e03
anaravanekar <anaravanekar@serenecorp.com>
2017-12-05 11:30:48 +0530

    internalaccountid

--- src/main/java/com/orchestranetworks/account/ui/form/AddressPane.java
+++ src/main/java/com/orchestranetworks/account/ui/form/AddressPane.java
@@ -49,10 +49,10 @@ public class AddressPane implements UIFormPane {
 				textToAppend.append("    ").append(context.getCurrentRecord().getString(_AccountName));
 			}*/
 			//textToAppend.append(context.getCurrentRecord()!=null?context.getCurrentRecord().getString(_):"");
-			String internalAccountId = StringUtils.isNotBlank(context.getCurrentRecord().getString(Paths._Account._InternalAccountId))?context.getCurrentRecord().getString(Paths._Account._InternalAccountId):null;
+			/*String internalAccountId = StringUtils.isNotBlank(context.getCurrentRecord().getString(Paths._Account._InternalAccountId))?context.getCurrentRecord().getString(Paths._Account._InternalAccountId):null;
 			if(internalAccountId!=null){
 				textToAppend.append("    Internal Account Id: ").append(internalAccountId);
-			}
+			}*/
 
 			if(StringUtils.isNotBlank(textToAppend.toString())) {
 				writer.addJS("appendToFormHeader(\"" + textToAppend + "\");");
@@ -209,12 +209,27 @@ public class AddressPane implements UIFormPane {
 			writer.add("</td>");
 			writer.add("</tr>");
 		}
-		writer.add("<tr><td colspan=\"1\" nowrap=\"nowrap\" style=\"" + CELL_STYLE_RIGHT + "\"><font color=\"#606060\">");
-		writer.addLabel(_RMTId);
-		writer.add("</td><td colspan=\"3\" style=\"" + CELL_STYLE_LEFT + "\">");
-		writer.addWidget(_RMTId);
-		writer.add("</td>");
-		writer.add("</tr>");
+		String internalAccountId = StringUtils.isNotBlank(context.getCurrentRecord().getString(Paths._Address._InternalAccountId))?context.getCurrentRecord().getString(Paths._Address._InternalAccountId):null;
+		if(internalAccountId==null) {
+			writer.add("<tr><td colspan=\"1\" nowrap=\"nowrap\" style=\"" + CELL_STYLE_RIGHT + "\"><font color=\"#606060\">");
+			writer.addLabel(_RMTId);
+			writer.add("</td><td colspan=\"3\" style=\"" + CELL_STYLE_LEFT + "\">");
+			writer.addWidget(_RMTId);
+			writer.add("</td>");
+			writer.add("</tr>");
+		}else{
+			writer.add("<tr><td colspan=\"1\" nowrap=\"nowrap\" style=\"" + CELL_STYLE_RIGHT + "\"><font color=\"#606060\">");
+			writer.addLabel(_RMTId);
+			writer.add("</td><td colspan=\"1\" style=\"" + CELL_STYLE_LEFT + "\">");
+			writer.addWidget(_RMTId);
+			writer.add("</td>");
+			writer.add("<td colspan=\"1\" nowrap=\"nowrap\" style=\"" + CELL_STYLE_RIGHT + "\"><font color=\"#606060\">");
+			writer.addLabel(_InternalAccountId);
+			writer.add("</td><td colspan=\"1\" style=\"padding-top:2px; "  + CELL_STYLE_LEFT + "\">");
+			writer.add("<b>"+internalAccountId+"</b>");
+			writer.add("</td>");
+			writer.add("</tr>");
+		}
 
 		if(!context.isCreatingRecord()) {
 			String mdmAccountId = context.getCurrentRecord()!=null?context.getCurrentRecord().getString(_MDMAccountId):null;
