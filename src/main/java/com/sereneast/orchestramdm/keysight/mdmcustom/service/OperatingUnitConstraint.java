90d25cec13d19296fea73f9a6512f33a43e127f3
anaravanekar <anaravanekar@serenecorp.com>
2018-04-19 23:34:54 +0530

    ou constraint

---
+++ src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/service/OperatingUnitConstraint.java
@@ -0,0 +1,76 @@
+package com.sereneast.orchestramdm.keysight.mdmcustom.service;
+
+import com.onwbp.adaptation.Adaptation;
+import com.onwbp.adaptation.AdaptationTable;
+import com.onwbp.adaptation.RequestResult;
+import com.onwbp.base.text.UserMessage;
+import com.orchestranetworks.instance.ValueContext;
+import com.orchestranetworks.instance.ValueContextForValidation;
+import com.orchestranetworks.schema.Constraint;
+import com.orchestranetworks.schema.ConstraintContext;
+import com.orchestranetworks.schema.ConstraintOnNull;
+import com.orchestranetworks.schema.Path;
+import com.sereneast.orchestramdm.keysight.mdmcustom.Paths;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+
+public class OperatingUnitConstraint implements Constraint, ConstraintOnNull {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OperatingUnitConstraint.class);
+
+    @Override
+    public void checkNull(ValueContextForValidation valueContextForValidation) {
+        LOGGER.debug("OperatingUnitConstraint.checkNull->");
+    }
+
+    @Override
+    public void checkOccurrence(Object o, ValueContextForValidation valueContextForValidation) {
+        LOGGER.debug("OperatingUnitConstraint.checkOccurrence->");
+        if(o!=null) {
+            LOGGER.debug("o not null");
+            List addressOus = (List)o;
+            LOGGER.debug("addressOus="+addressOus.size());
+            List<String> ousWithNoBp = new ArrayList<>();
+            AdaptationTable table = valueContextForValidation.getAdaptationInstance().getTable(Paths._BusinessPurpose.getPathInSchema());
+            RequestResult bpResult = table.createRequestResult(Paths._BusinessPurpose._MDMAddressId.format() + " = '" + valueContextForValidation.getValue(Path.PARENT.add(Paths._Address._MDMAddressId)) + "'");
+            HashSet<String> bpOus = new HashSet<>();
+            if (bpResult != null && !bpResult.isEmpty()) {
+                for (Adaptation bpAdapatation; (bpAdapatation = bpResult.nextAdaptation()) != null; ) {
+                    if (bpAdapatation.getList(Paths._BusinessPurpose._OperatingUnit) != null) {
+                        List<String> ous = bpAdapatation.getList(Paths._BusinessPurpose._OperatingUnit);
+                        if (ous != null) {
+                            bpOus.addAll(ous);
+                        }
+                    }
+                }
+            }
+            LOGGER.debug("bpOus="+bpOus.size());
+            for(Object addressOu:addressOus){
+                if(!bpOus.contains(String.valueOf(addressOu))){
+                    ousWithNoBp.add(String.valueOf(addressOu));
+                }
+            }
+            LOGGER.debug("ousWithNoBp="+ousWithNoBp.size());
+            if(!ousWithNoBp.isEmpty()){
+                UserMessage message = UserMessage.createError("No Business Purpose exists for Operating Units "+StringUtils.join(ousWithNoBp, ','));
+                valueContextForValidation.addMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void setup(ConstraintContext constraintContext) {
+
+    }
+
+    @Override
+    public String toUserDocumentation(Locale locale, ValueContext valueContext) {
+        return null;
+    }
+}
