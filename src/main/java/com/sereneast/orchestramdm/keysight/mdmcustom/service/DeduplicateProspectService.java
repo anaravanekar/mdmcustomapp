efa07d5e968b629f1f0109c0bd6956e474f5052f
anaravanekar <anaravanekar@serenecorp.com>
2018-06-27 17:51:04 +0530

    dedup prospect service

--- src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/service/DeduplicateProspectService.java
+++ src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/service/DeduplicateProspectService.java
@@ -182,18 +182,18 @@ public class DeduplicateProspectService implements UserService<TableViewEntitySe
         String finalMessage = "";
         try {
             deduplicateSfdcProspects(aContext);
-            anAjaxResponse.getWriter().add("<div class=\"custom-error-image\"></div>");
-            anAjaxResponse.getWriter().add("<div class=\"custom-error-header\">Success!</div>");
-            anAjaxResponse.getWriter().add("<div class=\"custom-error-message\">" + finalMessage);
+            anAjaxResponse.getWriter().add("<div class=\"custom-success-image\"></div>");
+            anAjaxResponse.getWriter().add("<div class=\"custom-success-header\">Oh No! Something went wrong.</div>");
+            anAjaxResponse.getWriter().add("<div class=\"custom-success-message\">" + finalMessage);
             anAjaxResponse.getWriter().add("</div>");
             String urlSfdcDs = anAjaxResponse.getWriter().getURLForSelection(Repository.getDefault().lookupHome(HomeKey.forBranchName("CMDReference")).findAdaptationOrNull(AdaptationName.forName("Prospect")));
             LOGGER.info("Url for Prospect datset : "+urlSfdcDs);
             //anAjaxResponse.getWriter().addJS("window.location.href='"+urlSfdcDs+"';");
         }catch(ApplicationRuntimeException e){
             finalMessage = e.getMessage();
-            anAjaxResponse.getWriter().add("<div class=\"custom-success-image\"></div>");
-            anAjaxResponse.getWriter().add("<div class=\"custom-success-header\">Oh No! Something went wrong.</div>");
-            anAjaxResponse.getWriter().add("<div class=\"custom-success-message\">" + finalMessage);
+            anAjaxResponse.getWriter().add("<div class=\"custom-error-image\"></div>");
+            anAjaxResponse.getWriter().add("<div class=\"custom-error-header\">Success!</div>");
+            anAjaxResponse.getWriter().add("<div class=\"custom-error-message\">" + finalMessage);
             anAjaxResponse.getWriter().add("<br> Stack trace : <br>" + ExceptionUtils.getStackTrace(e));
             anAjaxResponse.getWriter().add("</div>");
         }
