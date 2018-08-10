86facd82ea403e159e6b455c6383cb036cc2e8cf
anaravanekar <anaravanekar@serenecorp.com>
2018-08-10 13:58:00 +0530

    AssignedTo

--- src/main/java/com/orchestranetworks/account/ui/form/ProspectPane.java
+++ src/main/java/com/orchestranetworks/account/ui/form/ProspectPane.java
@@ -47,7 +47,6 @@ public class ProspectPane implements UIFormPane {
 			writer.add("<td class=\"ebx_Input\" colspan=\"1\">");writer.addWidget(_AssignedTo);writer.add("</td>");
 			writer.add("<td colspan=\"5\" style=\"width:25%;  vertical-align:top;\" nowrap=\"nowrap\" color=\"#606060\">");writer.addButtonJavaScript(new UIButtonSpecJSAction(buttonLabel,"saveAssignmentProspect('"+dataSpace+"',ebx_form_getValue(\""+writer.getPrefixedPath(_AssignedTo).format()+"\"),'account','"+systemId+"')"));writer.add("</td>");
 			writer.add("</tr>");
-			writer.endFormRow();
 		}else{
 			writer.addFormRow(Path.parse("./AssignedTo"));
 		}
