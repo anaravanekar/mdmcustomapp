73b674818cf864183d9308e40e39226ced5d7ffa
anaravanekar <anaravanekar@serenecorp.com>
2018-09-21 00:24:55 +0530

    sfdc credential configuration

--- src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/config/properties/ws/Sfdc.java
+++ src/main/java/com/sereneast/orchestramdm/keysight/mdmcustom/config/properties/ws/Sfdc.java
@@ -1,5 +1,23 @@
 package com.sereneast.orchestramdm.keysight.mdmcustom.config.properties.ws;
 
 public class Sfdc extends RestPropertiesCommon {
+    private String clientId;
+    private String clientSecret;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
 }
 
